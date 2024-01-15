c1333a3b8a5bc380147af42e0739166b568574f1
Samuel R Borges <sammy_rb@hotmail.com>
2024-01-15 15:55:11 -0300

    desafio concluido

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,48 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.ReportMinDTO;
+import com.devsuperior.dsmeta.dto.SummaryMinDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+
+    @Query(value = "SELECT new com.devsuperior.dsmeta.dto.ReportMinDTO(obj.id, obj.date, obj.amount, obj.seller.name) " +
+            "FROM Sale obj " +
+            "WHERE obj.date BETWEEN :min AND :max " +
+            "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name))",
+            countQuery = "SELECT COUNT(obj) " +
+                    "FROM Sale obj " +
+                    "JOIN obj.seller " +
+                    "WHERE obj.date BETWEEN :min AND :max " +
+                    "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name))")
+    Page<ReportMinDTO> searchSalesByPeriod(LocalDate min, LocalDate max, String name, Pageable pageable);
+
+    //versao selecionando tudo do obj e usando join fetch
+
+  /*  @Query(value = "SELECT obj " +
+            "FROM Sale obj " +
+            "JOIN FETCH obj.seller " +
+            "WHERE obj.date BETWEEN :min AND :max " +
+            "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name))",
+            countQuery = "SELECT COUNT(obj) " +
+                    "FROM Sale obj " +
+                    "JOIN obj.seller " +
+                    "WHERE obj.date BETWEEN :min AND :max " +
+                    "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name))")
+    Page<Sale> searchSalesByPeriod(LocalDate min, LocalDate max, String name, Pageable pageable);*/
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SummaryMinDTO(obj.seller.name, SUM(obj.amount)) " +
+            "FROM Sale obj " +
+            "WHERE obj.date BETWEEN :min AND :max " +
+            "GROUP BY obj.seller.name")
+    List<SummaryMinDTO> searchTotalSales(LocalDate min, LocalDate max);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,16 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.ReportMinDTO;
+import com.devsuperior.dsmeta.dto.SummaryMinDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -19,5 +27,32 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<ReportMinDTO> searchSalesByPeriod(String minDate, String maxDate, String name, Pageable pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+		LocalDate max = maxDate.equals("")? today : LocalDate.parse(maxDate);
+		LocalDate min = minDate.equals("")? today.minusYears(1L) : LocalDate.parse(minDate);
+
+		Page<ReportMinDTO> result = repository.searchSalesByPeriod(min, max, name, pageable);
+
+		//versao que seleciona tudo obj e recebe a entidade
+		//Page<Sale> result = repository.searchSalesByPeriod(min, max, name, pageable);
+		//return result.map(x -> new ReportMinDTO(x));
+
+		return result;
+
+	}
+
+	public List<SummaryMinDTO> searchTotalSales(String minDate, String maxDate) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+		LocalDate max = maxDate.equals("")? today : LocalDate.parse(maxDate);
+		LocalDate min = minDate.equals("")? today.minusYears(1L) : LocalDate.parse(minDate);
+
+		List<SummaryMinDTO> dto = repository.searchTotalSales(min, max);
+
+		return dto;
 	}
 }
